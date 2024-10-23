12b0d8f4778388994bb77e870585d961118b8bbc
DanielaEsposito <esposito.daniela9817@gmail.com>
2024-10-23 17:08:19 +0200

    add tiket js

--- main.js
+++ main.js
@@ -4,6 +4,11 @@ const fullNameImput = document.getElementById("input-fullname");
 const kmiImput = document.getElementById("input-km");
 const ageImput= document.getElementById("input-age");
 const formImput = document.getElementById("imput-form");
+const outputName =document.getElementById("name")
+const outputTiket =document.getElementById("tiket")
+const outputCarrozza = document.getElementById("carrozza")
+const outputPrice = document.getElementById("price")
+const outputCode = document.getElementById("code")
 //console.log(fullNameImput);
 //console.log(kmiImput);
 //console.log(ageImput);
@@ -24,25 +29,38 @@ console.log(priceForKm);
 formImput.addEventListener('submit', (event) => {
     event.preventDefault();
     const fullName = fullNameImput.value;
-    const km = kmiImput.value;
+    const km = kmiImput.value.parseInt();
     const age = ageImput.value;
+    //numero random per la carrozza
+    const carrozza = Math.floor(Math.random() * (10-1+1)+ 1);
+    //numero random per il codice del treno
+    const code = Math.floor(Math.random() * (9999-9000+1)+ 9000);
+    //calcolo del prezzo finale con lo sconto e annuncio che tipo di biglietto avrà l'utente
     const totalPriceForKm = km * priceForKm;
-    finalPrice=0;
+    const finalPrice=0;
+    const tiketType ="";
     if(age > 18){
         finalPrice = (totalPriceForKm * discount18) / 100 ;
+        tiketType ="Biglietto Ridotto";
     }
     else if(age > 65){
         finalPrice = (totalPriceForKm * discount65) / 100 ;
+        tiketType ="Biglietto Ridotto";
     }
     else{
         finalPrice = totalPriceForKm;
+        tiketType ="Biglietto Standard";
     }
-    const price = finalPrice.toFixed(2)
-    console.log(fullName);
-    console.log(age);
-    console.log(km) ;
-    console.log(finalPrice);
+   
+    //console.log(fullName);
+    //console.log(age);
+    //console.log(km) ;
+    //console.log(finalPrice);
     
-
+outputName.innerHTML= fullName;
+outputPrice.innerHTML= finalPrice.toFixed(2) + "€";
+outputCarrozza.innerHTML = carrozza;
+outputCode.innerHTML = code;
+outputTiket.innerHTML =tiketType;
 })
 
